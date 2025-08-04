da4d043e27a22dcfdc3cc8b66d9a5aebfefe6101
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:30:53 +0200

    Add files via upload

---
+++ 09 Weighted Graphs/Practice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Weighted_Graphs
+{
+    internal class Practice
+    {
+    }
+}
