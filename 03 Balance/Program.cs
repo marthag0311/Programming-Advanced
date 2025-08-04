ae5c4fd1f7ceeb9c90c6094d3cb21966ac8bf257
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:24:36 +0200

    Add files via upload

---
+++ 03 Balance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03_Balance
+{
+    internal class Program
+    {
+        /*
+        Given a string, check if it has balanced brackets. 
+        Balanced brackets mean that an opening bracket 
+        always has a corresponding closing symbol. You 
+        have to deal with the following brackets: ( ), [ ], < > and { }. 
+        The output must be one of the following sentences: 
+        'Not balanced expression', 'Balanced expression', 
+        'Too many close symbols' and 'Too many open symbols'.
+
+        Hint: work with a stack, because the last opened 
+        bracket must always be closed first (FILO).
+
+        Try to solve each exercise in different ways, always 
+        start from the naive approach first (brute force), and 
+        then try to work more efficiently based on what we saw 
+        in the lesson! Think what Big(O) could be!
+
+        CodeGrade Setup = .Net framework 6.0
+
+        Input/Output
+        input:
+        ([{{[()]}}]))
+        output:
+        Too many close symbols
+
+        input:
+        This {is} a balanced [string]!
+        output:
+        Balanced expression!
+         */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
