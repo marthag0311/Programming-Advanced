ca87b7524d59a5e3674e4b9cd1724ba229e8e76c
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:23:26 +0200

    Add files via upload

---
+++ 02 Sorting Principles, Algorithms & Applications/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02_Sorting_Principles__Algorithms___Applications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bubble Sort
+            int[] array = { 32, 7, 45, 23, 41, 19, 28, 50, 1, 37 };
+
+            Sorting sort = new Sorting();
+            sort.Bubble(array);
+
+        }
+    }
+}
---
+++ 02 Sorting Principles, Algorithms & Applications/Sorting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Sorting_Principles__Algorithms___Applications
+{
+    internal class Sorting
+    {
+        // Bubble Sort
+        //Scan the array, swapping adjacent pair of elements if they are not in  order. This bubbles up the largest element to the end.
+        //Scan the array again, bubbling up the second largest element.
+        //etc...repeat until all elements are sorted
+
+        public void Bubble(int[] array)
+        {
+            for (int i = 0; i <= array.Length; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    array[i] == array[i + 1];
+                    array[i + 1] == array[i];
+                }
+            }
+        }
+    }
+}
