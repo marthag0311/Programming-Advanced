b720f9177beae40ae820cd0934ad402885022177
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:18:21 +0200

    Add files via upload

---
+++ 01 Duplicates/Assignment.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Duplicates
+{
+    internal class Assignment
+    {
+        public string Duplicates(string start, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string line in words)
+            {
+                if (line.StartsWith(start))
+                {
+                    if (dict.ContainsKey(line))
+                    {
+                        dict[line]++;
+                    }
+                    else
+                    {
+                        dict[line] = 1;
+                    }
+                }
+            }
+
+            string unique = "";
+
+            foreach (var pair in dict)
+            {
+                unique += pair.Key + " ";
+            }
+
+            return unique.TrimEnd(); 
+        }
+        public string BruteForce(string start, string filename)
+        {
+            //Overall space complexity is O(n + k) = O(n)
+            //Overall time complexity is O(n + k) = O(n)
+            string[] words = File.ReadAllLines(filename); //time & space complexity = o(n)
+
+            Dictionary<string, int> dict = new Dictionary<string, int>(); //space complexity = O(k)
+
+            for (int i = 0; i < words.Length; i++) //time complexity = O(n)
+            {
+                if (words[i].StartsWith(start))
+                {
+                    if (!dict.ContainsKey(words[i])) dict[words[i]] = 1; //space complexity = O(1)
+                    else dict[words[i]]++; //space complexity = O(1)
+                }
+            }
+
+            List<string> unique = new List<string>(); //space complexity = O(k)
+
+            foreach (var pair in dict) //time complexity = O(k)
+            {
+                if (pair.Value > 1)
+                {
+                    unique.Add(pair.Key);
+                }
+            }
+
+            string output = "";
+
+            foreach (string item in unique) //time complexity = O(k)
+            {
+                output += item + " ";
+            }           
+            
+            return output;
+        }        
+    }
+}
---
+++ 01 Duplicates/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace _01_Duplicates
+{
+    internal class Program
+    {
+        /*
+        Find all duplicates starting with a given sequence 
+        in the given file Download the given file(already 
+        in CodeGrade). Try to solve each exercise in 
+        different ways, always start from the naive approach 
+        first (brute force), and then try to work more 
+        efficiently based on what we saw in the lesson! 
+        Think what Big(O) could be!
+
+        CodeGrade Setup = .Net framework 6.0
+
+        input:
+        aa
+        output:
+        aaaogvhosm aaarwdshpe aabetaeils aacfrlsdtd 
+        aacguuwgmu aaddhroonm aadgjyuviz aafajokdlu 
+        aafhakixki aaheyrnjzo aahkjrnvhr aahpixoayl 
+        aahxwxcizt aalkylujbh aamdipybjk aamqfpmnqu 
+        aaptqhdcwm aaqejjuict aaqnfgbglt aarfnsmwbd 
+        aarnlwpfce aauckqruos aaukjnnsnb aavjvxzqhn 
+        aavoilagcu aawwvajmny aaxktlbwye
+
+        input:
+        1
+        output:
+        No duplicates found.
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                string start = Console.ReadLine();
+
+                Assignment assignment = new Assignment();
+                //Console.WriteLine(assignment.Duplicates(start, "words.txt"));
+                //Console.WriteLine(assignment.BruteForce(start, "words.txt"));
+                Console.WriteLine(assignment.BinarySearch(start, "words.txt"));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
