aaab228b4c2cc62784e41a6fd854cf58cca0a9c6
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:32:52 +0200

    Add files via upload

---
+++ 04 Unique/Assignment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Unique
+{
+    internal class Assignment
+    {
+        public List<char> Unique(List<char> input, List<char> outcome, string temp = "")
+        {
+            if (input.Count == 0)
+            {
+                outcome.AddRange(temp);
+                return outcome;
+            }
+            else
+            {
+                if (input[0] == ' ')
+                {
+                    outcome.AddRange(temp);
+                    temp = "";
+                }
+
+                if (!temp.Contains(input[0]))
+                {
+                    temp += input[0];
+                }
+
+                input.RemoveAt(0);
+                return Unique(input, outcome, temp);
+            }
+        }
+
+        public void Print(List<char> unique)
+        {
+            foreach (var item in unique)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
---
+++ 04 Unique/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Unique
+{
+    internal class Program
+    {
+        /*
+        Read in a string and remove all the same adjacent characters.
+
+        Catch: you are not allowed to use loops or LINQ statements!
+
+ 
+
+        Input/Output
+        input:
+        aabbccdde eff
+        output:
+        abcde ef
+
+        input:
+        abcdefg
+        output:
+        abcdefg
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                List<char> letters = new List<char>();
+                List<char> outcome = new List<char>();
+
+                foreach (var item in input)
+                {
+                    letters.Add(item);
+                }
+
+                Assignment assignment = new Assignment();
+                assignment.Print(assignment.Unique(letters, outcome));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
