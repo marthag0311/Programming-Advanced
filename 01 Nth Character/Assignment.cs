89141806bca8c263eaa3036bb481e48da47cf808
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:20:16 +0200

    Add files via upload

---
+++ 01 Nth Character/Assignment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Nth_Character
+{
+    internal class Assignment
+    {
+        public string NthCharacter(int number, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+            string text = string.Join("", words);
+
+            if (number < 0)
+            {
+                return "Crazy input!";
+            }
+
+            char character = text[number];
+
+            return character.ToString();
+        }
+
+        public char BruteForce(int number, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+            string text = string.Join("", words);
+            char character = text[number];
+            return character;
+        }
+
+        public string LinearSearch(int number, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+            string text = string.Join("", words);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i == number) return text[i].ToString();
+            }
+            return "Crazy input!";
+        }
+
+        public string BinarySearch(int number, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+            string text = string.Join("", words);
+
+            int start = 0;
+            int end = text.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = end - start / 2;
+
+                if (mid == number) return text[mid].ToString();
+                else if (number > mid) start = mid + 1;
+                else end = mid - 1;
+            }
+
+            return "Crazy input!";
+        }
+
+        public string StupidSearch(int number, string filename)
+        {
+            string[] words = File.ReadAllLines(filename);
+            string text = string.Join("", words);
+
+            int start = 0;
+            int end = text.Length * 2;
+            Random random = new Random();
+
+            while (start <= end)
+            {
+                start++;
+                int index = random.Next(0, text.Length);
+
+                if (index == number) return text[index].ToString();
+            }
+            return "Crazy input!";
+        }
+    }
+}
---
+++ 01 Nth Character/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Nth_Character
+{
+    internal class Program
+    {
+        /*
+         Combine all words from the given file Download 
+        from the given file(already in CodeGrade) into 
+        one big string and find the nth character. Try 
+        to solve each exercise in different ways, always 
+        start from the naive approach first (brute force), 
+        and then try to work more efficiently based on 
+        what we saw in the lesson! Think what Big(O) could be!
+
+        CodeGrade Setup = .Net framework 6.0
+
+        input:
+        4728391
+        output:
+        h
+
+        input:
+        9256087
+        output:
+        k
+
+        Code instructions
+
+        To solve this assignment you need to create a 
+        Module_01 namespace with an Assignment class 
+        and a method NthCharacter
+        
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Assignment assignment = new Assignment();
+                //Console.WriteLine(assignment.NthCharacter(number, "words.txt"));
+                //Console.WriteLine(assignment.BruteForce(number, "words.txt"));
+                //Console.WriteLine(assignment.LinearSearch(number, "words.txt"));
+                //Console.WriteLine(assignment.BinarySearch(number, "words.txt"));
+                Console.WriteLine(assignment.StupidSearch(number, "words.txt"));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
