2dc0bc8594cc4ced7555e3d95ed2baab783073f3
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:35:28 +0200

    Add files via upload

---
+++ 06 Names/Assignment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Names
+{
+    class Assignment
+    {
+        public List<string> ListQuickSort(List<string> list)
+        {
+            if (list.Count <= 0) return list;
+
+            string pivot = list[0];
+
+            //"Martha","Geoffrey","Rozy","Alissa","Merina","Ananya"
+
+            List<string> smaller = new List<string>();
+            List<string> greater = new List<string>();
+
+            foreach (var item in list)
+            {
+                if (String.Compare(item, pivot) < 0) smaller.Add(item.ToLower()); // item < pivot
+                else if (String.Compare(item, pivot) > 0) greater.Add(item.ToLower()); // item > pivot
+            }
+            return ListQuickSort(smaller).Union(new List<string> { pivot }).Union(ListQuickSort(greater)).ToList<string>();
+        }
+
+        public int BinarySearch(List<string> list, string name)
+        {
+            int mid = 0;
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                
+                if (name == list[mid])
+                {
+                    return mid;
+                }
+                else if (String.Compare(name, list[mid]) < 0)
+                {
+                    end = mid - 1;
+                }
+                else if (String.Compare(name, list[mid]) > 0)
+                {
+                    start = mid + 1;
+                }
+            }
+            if (mid == 0) return 0;
+            else return mid + 1;
+        }
+    }
+}
---
+++ 06 Names/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Names
+{
+    internal class Program
+    {
+        /*
+        Given an (unsorted) file of names 
+        (names.txt already in CodeGrade). 
+        Read a name and find its (new) place (index) in the 
+        sorted name list.
+
+        input:
+        Elke
+        output:
+        5158
+
+        input:
+        Collin
+        output:
+        3353
+         */
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine().ToLower().Trim();
+            //string[] array = File.ReadAllLines("names.txt");
+            //List<string> list = new List<string>(array);
+
+            List<string> list = new List<string>(new int[] {"Martha","Geoffrey","Rozy","Alissa","Merina","Ananya" });
+
+            Assignment assignment = new Assignment();
+            List<string> sorted = assignment.ListQuickSort(list);
+            Console.WriteLine(string.Join(" ", sorted));
+            //int index = assignment.BinarySearch(sorted, name);
+            //Console.WriteLine(index);
+        }
+    }
+}
