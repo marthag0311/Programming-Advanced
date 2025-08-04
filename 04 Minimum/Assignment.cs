e0769f6398acbcb559f80cab8517cf014e461e3a
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:26:47 +0200

    Add files via upload

---
+++ 04 Minimum/Assignment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Minimum
+{
+    internal class Assignment
+    {
+        public int Minimum(List<int> list, int smallest)
+        {
+            if (list.Count == 0)
+            {
+                return smallest;
+            }
+            else
+            {
+                if (list[0] < smallest)
+                {
+                    smallest = list[0];
+                }
+                list.RemoveAt(0);
+                return Minimum(list, smallest);
+            }
+        }
+    }
+}
---
+++ 04 Minimum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Minimum
+{
+    internal class Program
+    {
+        /*
+        Find the minimum element in a given array.
+
+        Catch: you are not allowed to use loops or LINQ statements!
+
+        CodeGrade Setup = .Net framework 6.0
+
+
+        Input/Output
+        input:
+        1 2 5 4 7 8 1
+        output:
+        1
+
+        input:
+        5 6 9 8 7 4 10 0 2 -4 5 7
+        output:
+        -4
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] array = Console.ReadLine().Split();
+
+                List<int> input = new List<int>();
+
+                foreach (var item in array)
+                {
+                    input.Add(Convert.ToInt32(item));
+                }
+
+                Assignment assignment = new Assignment();
+
+                Console.WriteLine(assignment.Minimum(input, input[0]));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+        }
+    }
+}
