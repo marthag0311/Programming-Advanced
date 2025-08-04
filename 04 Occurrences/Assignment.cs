92bc53c3709c0550d3c86743b497ae5d9131549a
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:31:11 +0200

    Add files via upload

---
+++ 04 Occurrences/Assignment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Occurrences
+{
+    internal class Assignment
+    {
+        public int Occurences(List<int> numbers, int number, int count)
+        {
+            if (numbers.Count == 0)
+            {
+                return count;
+            }
+            else
+            {
+
+                if (numbers[0] == number)
+                {
+                    count++;
+                }
+                numbers.RemoveAt(0);
+
+                return Occurences(numbers, number, count);
+            }
+        }
+    }
+}
---
+++ 04 Occurrences/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Occurrences
+{
+    internal class Program
+    {
+        /*
+        Find the occurrence of a number (first line of input) in a sequence of numbers (second line of input)
+
+        Catch: you are not allowed to use loops or LINQ statements!
+
+
+        Input/Output
+        input:
+        8
+        7 8 5 4 1 2 8 8 9 6 5 4 8
+        output:
+        4
+
+        input:
+        0
+        0 0 0 0 4 5 8 7 4 0 0
+        output:
+        6
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                string[] input = Console.ReadLine().Split(" ");
+
+                List<int> numbers = new List<int>();
+                foreach (var item in input)
+                {
+                    numbers.Add(Convert.ToInt32(item));
+                }
+
+                Assignment assignment = new Assignment();
+                Console.WriteLine(assignment.Occurences(numbers, number, 0));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
