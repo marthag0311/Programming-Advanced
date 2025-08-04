5bc635a4ebe16d9f25aac087743eb42f3e9e8508
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:22:15 +0200

    Add files via upload

---
+++ 02 Largest/Assignment.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Largest
+{
+    internal class Assignment
+    {
+        public string Read(string input)
+        {
+            string[] sarray = input.Split(" ");
+
+            int[] array = Array.ConvertAll(sarray, int.Parse);
+
+            List<int> list = array.ToList();
+
+            Largest(list);
+
+            string largest = string.Join("", list);
+            return largest;
+
+            /*int max = array.Max();
+            int bucketsN = max + 1;
+
+            List<List<int>> buckets = new List<List<int>>(bucketsN);
+
+            for (int i = 0; i < bucketsN; i++)
+            {
+                buckets.Add(new List<int>());
+            }
+
+            foreach (int element in array)
+            {
+                int digit = element / 10; 
+                buckets[digit].Add(element);
+            }
+
+            int index = 0;
+            List<int> list = new List<int>();
+            foreach (List<int> bucket in buckets)
+            {
+                if (bucket != null)
+                {
+                    int[] insertion = Insertion(bucket.ToArray());
+
+                    foreach (int element in insertion)
+                    {
+                        array[index] = element;
+                        index++;
+                    }
+                }
+            }*/
+        }
+
+        static void Largest(List<int> list)
+        {
+            list.Sort((a, b) =>
+            {
+                string letterA = a.ToString();
+                string letterB = b.ToString();
+
+                string seqAB = letterA + letterB;
+                string seqBA = letterB + letterA;
+
+                return seqBA.CompareTo(seqAB); 
+            });
+        }
+    }
+}
---
+++ 02 Largest/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02_Largest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string start = Console.ReadLine();
+
+                Assignment assignment = new Assignment();
+                Console.WriteLine(assignment.Read(start));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
