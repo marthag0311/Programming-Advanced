8aa4b155b1d9407f07fcbd58739dfe3a994a9a85
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:30:44 +0200

    Add files via upload

---
+++ 04 Multiplication/Assignment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Multiplication
+{
+    internal class Assignment
+    {
+        public int Multiplication(int first, int second)
+        {
+            if (first == 0 || second == 0)
+            {
+                return 0;
+            }
+
+            if (second < 0)
+            {
+                return -Multiplication(first, -second);
+            }
+
+            if (second > 1)
+            {
+                return first + Multiplication(first, second - 1);
+            }
+
+            return first;
+        }
+    }
+}
---
+++ 04 Multiplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                if (!(input.Length > 2 || input.Length < 2))
+                {
+                    int[] array = Array.ConvertAll(input, Int32.Parse);
+
+                    int first = array[0];
+                    int second = array[1];
+
+                    Assignment assignment = new Assignment();
+                    Console.WriteLine(assignment.Multiplication(first, second));
+                }
+                else Console.WriteLine("Crazy input!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
