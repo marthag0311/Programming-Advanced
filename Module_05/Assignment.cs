6fba4e114c54601cee0fee7e27e0aa9634028fe6
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:36:28 +0200

    Add files via upload

---
+++ Module_05/Assignment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_05
+{
+    class Assignment
+    {
+        public string SubWords(string input)
+        {
+            string substrings = "";
+            for (int i = 1; i <= input.Length; i++)
+            {
+                for (int j = 0; j <= input.Length - i; j++)
+                {
+                    if (input.Substring(j, i).Length >= 1)
+                    {
+                        substrings += (input.Substring(j, i)) + " ";
+                    }
+                }
+            }
+            return substrings;
+        }
+
+        public List<string> Exists(string substrings, string[] text)
+        {
+            string[] sub = substrings.Split(" ");
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < sub.Length; i++)
+            {
+                if (text.Contains(sub[i].ToLower()))
+                {
+                    if (!result.Contains(sub[i].ToLower()))
+                    {
+                        result.Add(sub[i].ToLower());
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Module_05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                string[] text = File.ReadAllLines("english.txt");
+
+                Assignment assignment = new Assignment();
+                string subwords = assignment.SubWords(input);
+                List<string> exists = assignment.Exists(subwords, text);
+
+                Console.WriteLine(string.Join(" ", exists));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
