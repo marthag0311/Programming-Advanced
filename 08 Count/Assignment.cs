1053f3fd9dfff8427c1315b3a7e9ce130e6c7169
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:38:02 +0200

    Add files via upload

---
+++ 08 Count/Assignment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Count
+{
+    internal class Assignment
+    {
+        int nodes;
+        List<int>[] graph;
+
+        public Assignment(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new List<int>[nodes];
+
+            for (int i = 0; i < nodes; i++)
+            {
+                graph[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int node1, int node2)
+        {
+            graph[node1].Add(node2);
+            graph[node2].Add(node1);
+        }
+
+        public int Count()
+        {
+            bool[] visited = new bool[nodes];
+            int connected = 0;
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (!visited[i])
+                {
+                    DFS(i, visited);
+                    connected++;
+                }
+            }
+            return connected;
+        }
+
+        private void DFS(int start, bool[] visited)
+        {
+            visited[start] = true;
+
+            foreach (int neighbor in graph[start])
+            {
+                if (!visited[neighbor])
+                {
+                    DFS(neighbor, visited);
+                }
+            }
+        }
+    }
+}
---
+++ 08 Count/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+
+namespace _08_Count
+{
+    internal class Program
+    {
+        /*
+        Implement a Depth-First Search (DFS) algorithm to 
+        determine the number of connected components in a graph.
+
+        Input:
+        5 --> nr of nodes
+        3 --> nr of edges
+        0 1
+        2 3
+        3 4
+        Output:
+        2
+        Explanation:
+        There are two connected components in this graph: one 
+        containing nodes 0 and 1, and the other containing nodes 
+        2, 3, and 4.
+
+ 
+
+        Input:
+        4 --> nr of nodes
+        1 --> nr of edges
+        0 2
+        Output:
+        3
+        Explanation:
+        There are three connected components in this graph: one 
+        containing nodes 0 and 2, and two single-node components 
+        containing nodes 1 and 3.
+         */
+        static void Main(string[] args)
+        {
+            int nodes = Convert.ToInt32(Console.ReadLine());
+            int edges = Convert.ToInt32(Console.ReadLine());
+
+            Assignment assignment = new Assignment(nodes);
+
+            int index = 0;
+
+            while (index < edges)
+            {
+                string[] all = Console.ReadLine().Split(' ');
+                int[] integers = Array.ConvertAll(all, int.Parse);
+                assignment.AddEdge(integers[0], integers[1]);
+                index++;
+            }
+            Console.WriteLine(assignment.Count());
+        }
+    }
+}
