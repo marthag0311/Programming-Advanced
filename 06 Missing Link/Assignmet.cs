e007b67d98188e102ecc7e2fc9e18c6d03058611
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:34:41 +0200

    Add files via upload

---
+++ 06 Missing Link/Assignmet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_06
+{
+    internal class Assignment
+    {
+        public List<int> ListQuickSort(List<int> list)
+        {
+            if (list.Count <= 0) return list;
+
+            int pivot = list[0];
+
+            List<int> smaller = new List<int>();
+            List<int> greater = new List<int>();
+
+            foreach (var item in list)
+            {
+                if (item < pivot) smaller.Add(item);
+                else if (item > pivot) greater.Add(item);
+            }
+            return ListQuickSort(smaller).Union(new List<int> { pivot }).Union(ListQuickSort(greater)).ToList<int>();
+        }
+
+        public string MissingLink(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] + 1 != list[i + 1] && list[i] > 0)
+                {
+                    return (list[i] + 1).ToString();
+                }
+            }
+            return "No missing link";
+        }
+    }
+}
+
---
+++ 06 Missing Link/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] numbers = Console.ReadLine().Split(" ");
+                int[] array = Array.ConvertAll(numbers, int.Parse);
+
+                List<int> list = new List<int>(array);
+
+                Assignment assignment = new Assignment();
+                List<int> sorted = assignment.ListQuickSort(list);
+                string number = assignment.MissingLink(sorted);
+                Console.WriteLine(number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
