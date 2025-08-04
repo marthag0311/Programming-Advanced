3c8e6b1fb547085db5ce69458e7b4c66f6c2a010
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:37:30 +0200

    Add files via upload

---
+++ Module_07/Assignment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_07
+{
+    class Assignment
+    {
+        public string Sequence(int number)
+        {
+            if (number == 1) return "";
+
+            string seq = "";
+
+            if (number % 2 == 0)
+            {
+                number = number / 2;
+                seq = Sequence(number) + number + " ";
+            }
+            else
+            {
+                number = (3 * number) + 1;
+                seq = Sequence(number) + number + " ";
+            }
+            return seq;
+        }
+
+        public int Sum(int number, string seq)
+        {
+            string[] array = seq.Split(" ");
+
+            List<string> list = new List<string>();
+
+            int sum = number;
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (Int32.TryParse(array[i], out int n))
+                {
+                    sum += n;
+                }
+                list.Add(array[i]);
+            }
+            Console.Write(number + string.Join(" ", list) + "\n");
+            return sum;
+        }                
+    }
+}
---
+++ Module_07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Module_07
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                Assignment assignment = new Assignment();
+
+                if (input <= 0) Console.WriteLine("Crazy input!");
+                else
+                {
+                    string descend = assignment.Sequence(input);
+                    Console.WriteLine(assignment.Sum(input, descend));
+                }                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
