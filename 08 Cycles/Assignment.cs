8fd0d8058be956882f4bb953d0b9c58576d41ab5
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:38:48 +0200

    Add files via upload

---
+++ 08 Cycles/Assignment.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Cycles
+{
+    internal class Assignment
+    {
+        int nodes;
+        List<int>[] graph;
+
+        public Assignment(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new List<int>[nodes]; //create graph with nr of nodes
+
+            for (int i = 0; i < nodes; i++) //initialize all lists
+            {
+                graph[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int node1, int node2)
+        {
+            //to make it better, check if a node already is in the list, not going out of bounds
+            graph[node1].Add(node2);
+            //graph[node2].Add(node1);
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < nodes; i++)
+            {
+                s += i + " --> " + String.Join(" ", graph[i]) + "\n";
+            }
+            return s;
+        }
+
+        public bool IsCyclic()
+        {
+            List<int> visited = new List<int>();
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (!visited.Contains(i) && DFS(i, visited))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool DFS(int start, List<int> visited)
+        {
+            //If the node is already visited and is present
+            //in the recursion stack, then there is a cycle
+            if (visited.Contains(start)) return true;
+           
+            visited.Add(start);
+
+            foreach (int neighbor in graph[start])
+            {
+                if (DFS(neighbor, visited))
+                {
+                    return true;
+                }
+            }
+            visited.Remove(start); // Backtrack
+
+            return false;
+        }
+    }
+}
---
+++ 08 Cycles/Program.cs
@@ -0,0 +1,63 @@
+﻿ namespace _08_Cycles
+{
+    internal class Program
+    {
+        /*
+        Implement a Depth-First Search (DFS) algorithm to check if there's a cycle in a given directed graph. Read in the number of nodes, the number of edges, and a list of all edges. Output "cyclic" if there is a cycle in the graph, or "acyclic" if there isn't.
+
+        image.png
+        cyclic graph:
+        A <- B
+        |   >
+        >  /
+          C
+
+        acyclic graph:
+        A -> B
+        |    >
+        >   /
+          C
+
+        Input
+        3 --> nr of nodes
+        2 --> nr of edges
+        1 1
+        1 2
+        output
+        cyclic
+
+        Input
+        3 --> nr of nodes
+        3 --> nr of edges
+        0 1
+        1 2
+        0 2
+        output
+        acyclic
+         */
+        static void Main(string[] args)
+        {
+            int nodes = Convert.ToInt32(Console.ReadLine());
+            int edges = Convert.ToInt32(Console.ReadLine());
+
+            Assignment assignment = new Assignment(nodes);
+
+            int index = 0;
+
+            while (index < edges)
+            {
+                string all = Console.ReadLine();
+
+                //if (string.IsNullOrWhiteSpace(all)) break;
+
+                string[] strings = all.Split(' ');
+                int[] integers = Array.ConvertAll(strings, int.Parse);
+
+                assignment.AddEdge(integers[0], integers[1]);              
+            }
+
+            if (assignment.IsCyclic()) Console.WriteLine("cyclic");
+            else Console.WriteLine("acyclic");
+        }
+    }
+}
