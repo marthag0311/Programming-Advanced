a665e6f1efbb4820710f54006ea2a94856829c84
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:31:18 +0200

    Add files via upload

---
+++ 10 Balanced/BST.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Balanced
+{
+    public class BST
+    {
+        public Node Root { get; set; }
+
+        public BST()
+        {
+            Root = null;
+        }
+
+        public BST(int value)
+        {
+            Root = new Node(value);
+        }
+
+        public void Add(int value)
+        {
+            if (Root == null) Root = new Node(value);
+            else Add(Root, value);
+        }
+
+        public void Add(Node node, int value)
+        {
+            
+        }
+    }
+}
---
+++ 10 Balanced/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _10_Balanced
+{
+    internal class Program
+    {
+        /*
+        Check if a given Binary Search Tree (BST) is balanced. A tree is considered balanced if, for every node in the tree, the height difference between its left and right subtree does not exceed 1. The program should read the nodes, construct the BST, and then output 'balanced' if the tree is balanced, or 'not balanced' if it is not.
+
+        Input:
+        1 2 3 4 5
+        Output:
+        not balanced
+        Explanation:
+        Right of the node: 1 -> 2 -> 3 -> 4 -> 5
+
+        Input:
+        10 3 5 2 12 11
+        Output:
+        balanced
+        Explanation:
+        Left of the node: 10 -> 3 (-> 2 & -> 5)
+        Right of the node: 10 -> 12 (-> 11 & )
+         
+         */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
