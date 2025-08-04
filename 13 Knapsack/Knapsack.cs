71dfe3d30ddb0303b255aff8245c757cbc890aa0
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:35:23 +0200

    Add files via upload

---
+++ 13 Knapsack/Knapsack.cs
@@ -0,0 +1,24 @@
+﻿namespace _13_Knapsack
+{
+    public class Knapsack
+    {
+        private List<Item> items;
+        private int maxWeight;
+
+        public Knapsack(List<Item> list, int v)
+        {
+            items = list;
+            maxWeight = v;
+        }         
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var item in items)
+            {
+                s += item.ToString() + "\n";
+            }
+            return s;
+        }
+    }
+}
