4037f79defc657c530d97bdd67cf60e1dd4d08e2
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:34:13 +0200

    Add files via upload

---
+++ 11 Hash Tables/Book_KVP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Hash_Tables
+{
+    internal class Book_KVP
+    {
+
+        private KeyValuePair<string, double>[] book;
+        private int index = 0;
+
+        public Book_KVP(int items)
+        {
+            book = new KeyValuePair<string, double>[items];
+        }
+
+        public void AddItem(string key, double value)
+        {
+            KeyValuePair<string, double> item = new KeyValuePair<string, double>(key, value);
+            book[index] = item;
+            index++;
+        }
+
+        internal double GetPrice(string v)
+        {
+            foreach (var item in book)
+            {
+                if (item.Key == v) return item.Value;
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Empty;
+            for (int i = 0; i < index; i++)
+            {
+                s += book[i].Key + ": " + book[i].Value + "\n";
+            }
+            return s;
+        }
+    }
+}
---
+++ 11 Hash Tables/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _11_Hash_Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Book_KVP book = new Book_KVP(20);
+            book.AddItem("apple", 0.67);
+            book.AddItem("pear", 0.79);
+            book.AddItem("eggs", 2.49);
+            book.AddItem("milk", 1.49);
+            book.AddItem("avocado", 1.49);
+            Console.WriteLine(book.ToString());
+            Console.WriteLine("Price of eggs: " + book.GetPrice("eggs"));
+            Console.WriteLine(book.ToString());*/
+
+            Book_Hash book = new Book_Hash(5);
+            Console.WriteLine(book.GetSize());
+            book.AddItem("apple", 0.67);
+            book.AddItem("pear", 0.79);
+            book.AddItem("eggs", 2.49);
+            book.AddItem("milk", 1.49);
+            book.AddItem("avocado", 1.49);
+            book.AddItem("kiwi", 4.49);
+            book.AddItem("chocolate", 7.49);
+            book.AddItem("bread", 2.37);
+            Console.WriteLine(book.ToString());
+            Console.WriteLine("Price of eggs: " + book.GetPrice("eggs"));
+            Console.WriteLine(book.ToString());
+        }
+    }
+}
