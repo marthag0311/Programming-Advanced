adbdab4932d4a2d25fcb8a73cb777bb3958fd4a3
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:30:20 +0200

    Add files via upload

---
+++ 09 Longest Path/Maze.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Longest_Path
+{
+    internal class Maze
+    {
+        int nodes;
+        List<KeyValuePair<int, int>>[] graph;
+        public Maze(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new List<KeyValuePair<int, int>>[nodes];
+
+            for (int i = 0; i < nodes; i++)
+            {
+                graph[i] = new List<KeyValuePair<int, int>>();
+            }
+        }
+
+        public void AddEdge(int node1, int node2, int weight)
+        {
+            graph[node1].Add(new KeyValuePair<int, int>(node2, weight));
+        }
+
+        public void LongestPath(int start)
+        {
+            int[] distances = new int[nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                distances[i] = Int32.MinValue; // Initialize distances to negative infinity
+            }
+
+            int[] previous = new int[nodes];
+            List<int> visited = new List<int>();
+
+            distances[start] = 0;
+
+            while (true)
+            {
+                int next = GetNextNode(distances, visited);
+
+                if (next == -1) break;
+
+                visited.Add(next);
+
+                foreach (KeyValuePair<int, int> edge in graph[next])
+                {
+                    int neighbour = edge.Key;
+                    int weight = edge.Value;
+
+                    int cost = distances[next] + weight;
+                    if (distances[neighbour] < cost)
+                    {
+                        distances[neighbour] = cost;
+                        previous[neighbour] = next;
+                    }
+                }
+            }
+            int maxDistance = Int32.MinValue;
+            for (int i = 0; i < nodes; i++)
+            {
+                if (distances[i] > maxDistance)
+                {
+                    maxDistance = distances[i];
+                }
+            }
+            Console.WriteLine(maxDistance);
+        }
+
+        private int GetNextNode(int[] distances, List<int> visited)
+        {
+            int next = -1;
+            int max = Int32.MinValue;
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (distances[i] > max && !visited.Contains(i))
+                {
+                    max = distances[i];
+                    next = i;
+                }
+            }
+            return next;
+        }
+    }
+}
---
+++ 09 Longest Path/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _09_Longest_Path
+{
+    internal class Program
+    {
+        /*
+        Find the longest path in the given Directed 
+        Acyclic Graph. Always start your search from 
+        the first node!
+
+        Input
+        4 --> nr of nodes
+        4 --> nr of edges
+        0,1,2 --> node 1, node 2, weight
+        1,2,3
+        2,3,4
+        0,2,1
+        output
+        9
+
+        Input
+        5 --> nr of nodes
+        6 --> nr of edges
+        0,1,3
+        1,2,4
+        0,2,2
+        2,3,6
+        1,3,5
+        3,4,2
+        output
+        15
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                int nodes = Convert.ToInt32(Console.ReadLine());
+                int edges = Convert.ToInt32(Console.ReadLine());
+
+                Maze maze = new Maze(nodes);
+                int index = 0;
+                while (index < edges)
+                {
+                    string[] array = Console.ReadLine().Split(",");
+                    int[] ints = Array.ConvertAll(array, int.Parse);
+                    maze.AddEdge(ints[0], ints[1], ints[2]);
+                    index++;
+                }
+                maze.LongestPath(0);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy Input!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Crazy Input!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Crazy Input!");
+            }            
+        }
+    }
+}
