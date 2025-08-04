a2f7472494265f0e695ecb49b1634b03bca1b628
marthag0311 <marageofrey@gmail.com>
2025-08-04 22:32:30 +0200

    Add files via upload

---
+++ 04 Sum/Assignment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Sum
+{
+    internal class Assignment
+    {
+        public int Sum(List<char> chars, int sum = 0)
+        {
+            if (chars.Count == 0)
+            {
+                return sum;
+            }
+            else
+            {
+                if (chars[0] == '1' || chars[0] == '2' || chars[0] == '3' || chars[0] == '4'
+                    || chars[0] == '5' || chars[0] == '6' || chars[0] == '7' || chars[0] == '8' || chars[0] == '9')
+                {
+                    sum += Convert.ToInt32(chars[0] - '0');
+                }
+                chars.RemoveAt(0);
+                return Sum(chars, sum);
+            }
+
+        }
+    }
+}
---
+++ 04 Sum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Sum
+{
+    internal class Program
+    {
+        /*
+        Read in a word and add up all the digits
+
+        Catch: you are not allowed to use loops or LINQ statements!
+
+ 
+
+        Input/Output
+        input:
+        123456789 are all digits
+        output:
+        45
+
+        input:
+        There are no digits
+        output:
+        0
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                List<char> chars = new List<char>();
+
+                foreach (var item in input)
+                {
+                    chars.Add(item);
+                }
+
+                Assignment assignment = new Assignment();
+                Console.Write(assignment.Sum(chars));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
